e4c0c7c8eaee9f9b67d023283fed62925e195c45
rostik3 <rostikeden@gmail.com>
2019-06-19 19:40:01 +0300

    @Rostik3 add form-validation, sizing elements

--- src/js/index.js
+++ src/js/index.js
@@ -17,3 +17,61 @@ var mySwiper = new Swiper ('.swiper-container', {
     slidesPerView: 5,
     spaceBetween: -74
 })
+
+$('#form__phone').mask('+38(999) 999-99-99');
+
+$('#orderForm').validate({
+    rules: {
+        name: {
+            required: true,
+        },
+        email: {
+            required: true,
+        },
+        phone: {
+            required: true
+        }
+    },
+    messages: {
+        name: {
+            required: 'Name field is required.'
+        },
+        email: {
+            required: 'Email field is required'
+        },
+        phone: {
+            required: 'Phone number field is required.'
+        }
+    }
+});
+
+    $(function () {
+        $("#orderForm").submit(function (event) {
+            const formName = $('#form__name').val();
+            const formEmail = $('#form__email').val();
+            const formPhone = $('#form__phone').val();
+            event.preventDefault();
+            if (formName && formEmail && formPhone) {
+                let data = {
+                    message: `
+	                Заказ индивидуальной консультации:
+	                Имя - ${formName},
+	                Email - ${formEmail},
+	                Телефон - ${formPhone},
+	                `,
+                };
+                $.ajax({
+                    type: "POST",
+                    url: "js/email.php",
+                    data: data,
+                    success: function () {
+                        alert(`Заявка принята`);
+                    }
+                });
+            } else {
+                alert("Заполните пожалуйста форму!")
+            }
+
+            return false;
+        });
+    });
